b0af2014b21278606051b664814fef9338fc6387
Mario Novella <mario.novella@gmail.com>
2022-12-07 11:35:24 +0100

    Manage successful signups

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,13 +5,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
-                                 email: "user@invalid",
-                                 password: "foo",
-                                 password_confirmation: "bar" } }
-      end
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+              end
     assert_response :unprocessable_entity
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
-  end 
+  end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    refute_empty flash
+  end
 end
