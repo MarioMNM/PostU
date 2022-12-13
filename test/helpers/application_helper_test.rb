79bb56616216a65845744a19504ebad050bc7e88
Mario <mario.novella@gmail.com>
2022-12-13 12:20:44 +0000

    Heroku email in production and first step of micropost

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal 'Ruby on Rails Tutorial Sample App', full_title
-    assert_equal 'Help | Ruby on Rails Tutorial Sample App', full_title("Help")
+    assert_equal 'PostU', full_title
+    assert_equal 'Help | PostU', full_title("Help")
   end
 end
