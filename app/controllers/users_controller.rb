5fc988dc920833fbefdff92cccad5159b89b3ec4
Mario <mario.novella@gmail.com>
2022-12-14 16:22:32 +0000

    Change to sign up flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Please check your email to activate your account. Check your Spam box in case you don't receive it."
       redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
