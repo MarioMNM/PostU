9300be78de631c63418514f6105cfe495afe2f94
Mario <mario.novella@gmail.com>
2022-12-22 16:45:48 +0000

    Cannot like more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,11 @@ class LikesController < ApplicationController
    before_action :find_post
    
   def create
-    @micropost.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @micropost.likes.create(user_id: current_user.id)
+    end
     redirect_to micropost_path(@micropost)
   end
   
@@ -10,5 +14,10 @@ class LikesController < ApplicationController
   
     def find_post
       @micropost = Micropost.find(params[:micropost_id])
+    end
+    
+    def already_liked?
+      Like.where(user_id: current_user.id, micropost_id:
+                 params[:micropost_id]).exists?
     end
 end
