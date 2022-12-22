08bcf2b986713f6148c211e27b62fed79e640132
Mario <mario.novella@gmail.com>
2022-12-22 17:09:23 +0000

    Add unlike feature and styling

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,21 @@
 class LikesController < ApplicationController
    before_action :find_post
+   before_action :find_like, only: [:destroy]
    
   def create
     if already_liked?
       flash[:notice] = "You can't like more than once"
     else
       @micropost.likes.create(user_id: current_user.id)
+    end
+    redirect_to micropost_path(@micropost)
+  end
+  
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
     end
     redirect_to micropost_path(@micropost)
   end
@@ -19,5 +29,9 @@ class LikesController < ApplicationController
     def already_liked?
       Like.where(user_id: current_user.id, micropost_id:
                  params[:micropost_id]).exists?
+    end
+    
+    def find_like
+      @like = @post.likes.find(params[:id])
     end
 end
