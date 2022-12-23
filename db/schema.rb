e92c339f272c625a5b53ae76ee401e0796ab2bc0
Mario <mario.novella@gmail.com>
2022-12-23 10:42:16 +0000

    Add descriptions to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_22_162553) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_101322) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_162553) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
